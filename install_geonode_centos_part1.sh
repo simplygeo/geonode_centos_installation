51689ba2e0ce28e8418a33c32aae64bf15af7f1e
Simply Geospatial <simplygeospatial@gmail.com>
2021-11-15 18:57:21 +0700

    Executable flag on docker-compose

--- install_geonode_centos_part1.sh
+++ install_geonode_centos_part1.sh
@@ -6,6 +6,7 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo dnf -y --nobest --allowerasing install docker-ce docker-ce-cli containerd.io
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64 -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 
 sudo systemctl start docker
 sudo usermod -aG docker $USER
